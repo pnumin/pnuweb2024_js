0ba2052bba70077c45cc8beb16491df215a71b3f
pnumin <pnumin@pusan.ac.kr>
2024-06-27 12:36:08 +0900

    기본 CSS 만들기

---
+++ 03/03.js
@@ -0,0 +1,20 @@
+
+// document.addEventListener("DOMContentLoaded", function () { 
+//   console.log('안녕!!')
+// }) ;
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  console.log("화살표 함수!!")
+
+  //변수선언
+  // const msg = document.getElementById('msg') ;
+  const msg = document.querySelector('#msg');
+  const bt = document.querySelector('button') ;
+
+  // msg.innerHTML = '<h1>안녕</h1>' ;
+  // msg.textContent = '<h1>안녕</h1>' ;
+
+  bt.addEventListener("click", ()=>{
+    msg.innerHTML = '<h1>안녕</h1>' ;
+  });
+});
